5c93fafa5acbfbe8486c140d83b4f5bddd456692
Nurmakhan Pazylkhan <nurmakhan_pazylkhan@mail.ru>
2022-07-28 17:30:47 +0600

    Add project files.

---
+++ WebAppMVCUsingMapper/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using WebAppMVCUsingMapper.Models;
+using WebAppMVCUsingMapper.ViewModels;
+
+namespace WebAppMVCUsingMapper.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IMapper _mapper;
+
+        public UserController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        public IActionResult Index()
+        {
+            var user = GetUserDetails();
+
+            UserViewModel userViewModel = _mapper.Map<UserViewModel>(user);
+            return View(userViewModel);
+        }
+
+        private static User GetUserDetails()
+        {
+            return new User()
+            {
+                Id = 1,
+                FirstName = "John",
+                LastName = "Smith",
+                Email = "John.Smith@gmail.com",
+                Address = new Address() 
+                { 
+                    City = "US"
+                }
+            };
+        }
+    }
+}
---
+++ WebAppMVCUsingMapper/MappingConfigurations/UserProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using WebAppMVCUsingMapper.Models;
+using WebAppMVCUsingMapper.ViewModels;
+
+namespace WebAppMVCUsingMapper.MappingConfigurations
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            // Default mapping when property names are same
+            CreateMap<User, UserViewModel>();
+
+            // Mapping when property names are different
+            //CreateMap<User, UserViewModel>()
+            //    .ForMember(dest =>
+            //    dest.FName,
+            //    opt => opt.MapFrom(src => src.FirstName))
+            //    .ForMember(dest =>
+            //    dest.LName,
+            //    opt => opt.MapFrom(src => src.LastName));
+        }
+    }
+}
